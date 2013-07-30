196a5b87070a28e0ae025d8c4b777ab19a770c97
Joseph Simoes <josephsimoes@new-host-2.home>
2013-07-30 14:49:51 -0400

    geocoder and google maps properly mapping event locations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :address, :description, :latitude, :longitude, :name, :image
   geocoded_by :address
+  after_validation :geocode
 
   has_attached_file :image, presence: true,
   													size: { less_than: 5.megabytes },
